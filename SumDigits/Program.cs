b4f8860a0f77a840fd23fc503d0935d933c397c1
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-16 23:45:23 +0300

    Initial commit

---
+++ SumDigits/Program.cs
@@ -0,0 +1,20 @@
+﻿class SumDigits
+{
+    public static void Main(string[] args)
+    {
+
+        int number = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+
+
+        while (number > 0)
+        {
+           int sumDigits = number % 10;
+           sum += sumDigits;
+            number /= 10;
+        }
+
+        Console.WriteLine($"{sum}");
+    }
+}
